3ce8ece103ab55199105596a7ff2d46e8aba1f80
Travis Lamberte <travis@lamberte.com>
2026-02-19 09:59:25 -0800

    merging 2 tables

--- practice_queries.sql
+++ practice_queries.sql
@@ -1,3 +1,7 @@
 SELECT first_name, last_name, signup_date
 FROM practice.customers
 WHERE signup_date BETWEEN '2024-03-01' AND '2024-03-31';
+
+SELECT *
+FROM practice.customers
+WHERE signup_date BETWEEN '2024-03-01' AND '2024-03-31';
