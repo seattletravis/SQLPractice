b16d9ebd9c73e228f43b5da08d1371683111ede1
Travis Lamberte <travis@lamberte.com>
2026-02-22 08:16:44 -0800

    create templates for adding food data

--- diet_data.sql
+++ diet_data.sql
@@ -1,2 +1,26 @@
-INSERT INTO food_log (user_id, log_date, meal, item, quantity, calories, notes)
-VALUES (1, CURRENT_DATE, 'Dinner', 'Steak', '1 steak (8 oz)', 600, 'Estimated calories for an 8 oz ribeye');
+-- INSERT INTO food_log (user_id, log_date, meal, item, quantity, calories, notes)
+-- VALUES (1, CURRENT_DATE, 'Dinner', 'Steak', '1 steak (8 oz)', 600, 'Estimated calories for an 8 oz ribeye');
+
+-- SELECT * FROM food_log;
+
+-- DELETE FROM food_log WHERE id = 2;
+
+
+
+
+
+
+-- BREAKFAST TEMPLATE
+-- INSERT INTO food_log (user_id, log_date, meal, item, quantity, calories, notes)
+-- VALUES (1, CURRENT_DATE, 'Breakfast', '', '1', 300, '');
+-- SELECT * FROM food_log;
+
+-- LUNCH TEMPLATE
+-- INSERT INTO food_log (user_id, log_date, meal, item, quantity, calories, notes)
+-- VALUES (1, CURRENT_DATE, 'Lunch', '', '1', 300, '');
+-- SELECT * FROM food_log;
+
+-- LUNCH TEMPLATE
+-- INSERT INTO food_log (user_id, log_date, meal, item, quantity, calories, notes)
+-- VALUES (1, CURRENT_DATE, 'Dinner', '', '1', 300, '');
+-- SELECT * FROM food_log;
